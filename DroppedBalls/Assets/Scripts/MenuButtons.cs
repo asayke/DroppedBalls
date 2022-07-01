35e1f884d44c31b0a8f1ab0c36f942c590619009
oleja <noilzep@gmail.com>
2022-07-01 21:02:33 +0300

    Little Menu

---
+++ DroppedBalls/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+   public void Play()
+   {
+      SceneManager.LoadScene("GameScene");
+   }
+
+   public void Exit()
+   {
+      Application.Quit();
+   }
+}
