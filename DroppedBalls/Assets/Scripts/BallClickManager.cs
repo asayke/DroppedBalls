5f9ca7a8e17e4a0c8a492cd28d83e080a6a7d858
oleja <noilzep@gmail.com>
2022-07-02 18:49:51 +0300

    FixedInifynityCoins

--- DroppedBalls/Assets/Scripts/BallClickManager.cs
+++ DroppedBalls/Assets/Scripts/BallClickManager.cs
@@ -6,20 +6,26 @@ public class BallClickManager : MonoBehaviour
     private TextMeshProUGUI sessionCoinsText;
     private PlayerStats playerStats;
     private Ball ball;
+    private HealhPoints points;
 
     private void Start()
     {
+        points = FindObjectOfType<HealhPoints>();
         sessionCoinsText = GameObject.Find("SessionCoims").gameObject.GetComponent<TextMeshProUGUI>();
         playerStats = FindObjectOfType<PlayerStats>();
         ball = gameObject.GetComponent<Ball>();
     }
 
+
     private void OnMouseDown()
     {
-        int thisBallCoins = ball.Coins();
-        playerStats.IncAllCoins(thisBallCoins);
-        playerStats.sessionCoins += thisBallCoins;
-        sessionCoinsText.SetText(playerStats.sessionCoins.ToString());
-        Destroy(gameObject, .1f);
+        if (points.CurrentHealthPoint() <= 3)
+        {
+            int thisBallCoins = ball.Coins();
+            playerStats.IncAllCoins(thisBallCoins);
+            playerStats.sessionCoins += thisBallCoins;
+            sessionCoinsText.SetText(playerStats.sessionCoins.ToString());
+            Destroy(gameObject, .1f);
+        }
     }
 }
