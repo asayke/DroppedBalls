ad8625185016ff39fff0b5b6a71867042acf2a07
oleja <noilzep@gmail.com>
2022-07-02 16:49:31 +0300

    TextBugFixed

--- DroppedBalls/Assets/Scripts/PlayerStats.cs
+++ DroppedBalls/Assets/Scripts/PlayerStats.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI allCoinsText;
+    private TextMeshProUGUI allCoinsText;
     private int heathPoint = 3;
-    
+
     private static int allCoins;
     public int sessionCoins;
-    
+
     public void IncAllCoins(int coins)
     {
         allCoins += coins;
@@ -17,10 +17,10 @@ public class PlayerStats : MonoBehaviour
 
     private void Start()
     {
+        GameObject allCoinsTextOnbj = GameObject.Find("TextCanvas/AllCoimsText");
+        allCoinsText = allCoinsTextOnbj.GetComponent<TextMeshProUGUI>();
         DontDestroyOnLoad(allCoinsText);
         DontDestroyOnLoad(gameObject);
         allCoinsText.SetText(allCoins.ToString());
     }
-    
-    
 }
